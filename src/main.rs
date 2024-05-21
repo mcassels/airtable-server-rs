50e4ceade96983dedb61ce0f0c760f3ac469a9d6
Morgan Brooks <morgancassels@gmail.com>
2024-05-21 08:17:04 -0700

    add cors handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use airtable_api::Record;
-use axum::{routing::get, Json, Router};
+use axum::{http::Method, routing::get, Json, Router};
+use tower_http::cors::{Any, CorsLayer};
 use dotenv::dotenv;
 use lazy_static::lazy_static;
 use mini_moka::sync::Cache;
@@ -25,7 +26,14 @@ async fn main() {
     // Load the .env file into the environment.
     dotenv().ok();
 
-    let app = Router::new().route("/table/:id", get(get_table_handler));
+    let cors = CorsLayer::new()
+      .allow_methods([Method::GET])
+      // allow requests from any origin
+      .allow_origin(Any);
+
+    let app = Router::new()
+      .route("/table/:id", get(get_table_handler))
+      .layer(cors);
 
     // fly.io requires this port
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
